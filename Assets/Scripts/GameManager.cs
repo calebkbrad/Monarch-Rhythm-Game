3378e49038a245c2c8d350fff0b1e306d2a63435
calebkbrad <92956463+calebkbrad@users.noreply.github.com>
2021-10-23 18:29:56 -0400

    Added score system
    Added a quick and dirty score system that has a basic multiplier system

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
-{
+{    
     public AudioSource theMusic;
 
     public bool startPlaying;
@@ -12,22 +13,34 @@ public class GameManager : MonoBehaviour
 
     public static GameManager instance;
 
+    public int currentScore;
+    public int scorePerNote = 100;
+    public int scoreMulti = 1;
+
+    public Text scoreText;
+    public Text multiText;
+
     // Start is called before the first frame update
     void Start()
     {
+        // Initialize reference to self
         instance = this;
+        // Initialize appropriate score texts
+        scoreText.text = "Score: 0";
+        multiText.text = "Multiplier: x1";
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Don't start playing until input is received
         if(!startPlaying)
         {
             if(Input.anyKeyDown)
             {
+                // Start the game
                 startPlaying = true;
                 bs.hasStarted = true;
-
                 theMusic.Play();
             }
         }
@@ -36,10 +49,19 @@ public class GameManager : MonoBehaviour
     public void NoteHit()
     {
         Debug.Log("Hit on time!");
+        // Increment and update multiplier
+        scoreMulti += 1;
+        multiText.text = "Multiplier: x" + scoreMulti;
+        // Increment and update score
+        currentScore += scorePerNote * scoreMulti;
+        scoreText.text = "Score: " + currentScore;
     }
 
     public void NoteMissed()
     {
         Debug.Log("You missed!");
+        // Reset the multiplier
+        scoreMulti = 1;
+        multiText.text = "Multiplier: x" + scoreMulti;
     }
 }
