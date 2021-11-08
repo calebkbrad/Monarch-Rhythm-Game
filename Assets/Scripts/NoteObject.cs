a4c82e0539f70d8aec96f83a3ed26ac7e0240568
calebkbrad <92956463+calebkbrad@users.noreply.github.com>
2021-11-07 23:18:19 -0500

    Improved scoring system
    Added point thresholds, and higher scores for better hits

--- Assets/Scripts/NoteObject.cs
+++ Assets/Scripts/NoteObject.cs
@@ -9,6 +9,8 @@ public class NoteObject : MonoBehaviour
 
     public KeyCode keyToPress;
 
+    public GameObject hitEffect, goodEffect, perfectEffect, missEffect;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,16 +31,19 @@ public class NoteObject : MonoBehaviour
                 {
                     Debug.Log("Hit");
                     GameManager.instance.NormalHit();
+                    Instantiate(hitEffect, transform.position, hitEffect.transform.rotation);
                 }
                 else if(Mathf.Abs(transform.position.y) > 0.05)
                 {
                     Debug.Log("Good Hit");
                     GameManager.instance.GoodHit();
+                    Instantiate(goodEffect, transform.position, goodEffect.transform.rotation);
                 }
                 else 
                 {
                     Debug.Log("Perfect!");
                     GameManager.instance.PerfectHit();
+                    Instantiate(perfectEffect, transform.position, perfectEffect.transform.rotation);
                 }
             }
         }
